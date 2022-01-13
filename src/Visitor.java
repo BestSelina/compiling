bcd7ff78221d111c085804471f1f4c860394db38
BestSelina <70215461+BestSelina@users.noreply.github.com>
2022-01-13 17:27:10 +0800

    Add files via upload

--- src/Visitor.java
+++ src/Visitor.java
@@ -77,43 +77,29 @@ public class Visitor extends miniSysYBaseVisitor<String> {
         }
         System.out.println("\tret i32 " + stack.pop());
 
-//        for (String s: expList){
-//            System.out.print(s);
-//        }
-//        System.out.print(exp);
         return null;
     }
 
     public List<String> parseToSuffixExpression(List<String> expressionList){
-        //创建一个栈用于保存操作符
         Stack<String> opStack = new Stack<>();
-        //创建一个list用于保存后缀表达式
         List<String> suffixList = new ArrayList<>();
         for(String item : expressionList){
-            //得到数或操作符
             if(isOperator(item)){
-                //是操作符 判断操作符栈是否为空
                 if(opStack.isEmpty() || "(".equals(opStack.peek()) || priority(item) > priority(opStack.peek())){
-                    //为空或者栈顶元素为左括号或者当前操作符大于栈顶操作符直接压栈
                     opStack.push(item);
                 }else {
-                    //否则将栈中元素出栈如队，直到遇到大于当前操作符或者遇到左括号时
                     while (!opStack.isEmpty() && !"(".equals(opStack.peek())){
                         if(priority(item) <= priority(opStack.peek())){
                             suffixList.add(opStack.pop());
                         }
                     }
-                    //当前操作符压栈
                     opStack.push(item);
                 }
             }else if(isNumber(item)){
-                //是数字则直接入队
                 suffixList.add(item);
             }else if("(".equals(item)){
-                //是左括号，压栈
                 opStack.push(item);
             }else if(")".equals(item)){
-                //是右括号 ，将栈中元素弹出入队，直到遇到左括号，左括号出栈，但不入队
                 while (!opStack.isEmpty()){
                     if("(".equals(opStack.peek())){
                         opStack.pop();
@@ -124,7 +110,6 @@ public class Visitor extends miniSysYBaseVisitor<String> {
                 }
             }
         }
-        //循环完毕，如果操作符栈中元素不为空，将栈中元素出栈入队
         while (!opStack.isEmpty()){
             if(!opStack.peek().equals("("))
             suffixList.add(opStack.pop());
@@ -132,17 +117,14 @@ public class Visitor extends miniSysYBaseVisitor<String> {
         return suffixList;
     }
 
-    //判断字符串是否为操作符
     public static boolean isOperator(String op){
         return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/") || op.equals("%");
     }
 
-    //判断是否为数字
     public static boolean isNumber(String num){
         return num.matches("\\d+");
     }
 
-    //获取操作符的优先级
     public static int priority(String op){
         if(op.equals("*") || op.equals("/") || op.equals("%")){
             return 1;
